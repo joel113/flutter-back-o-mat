a0b638d1a612de3894cd6a12eb46dcc223cf6b62
Johannes Ehm <johannes.ehm@gmail.com>
2022-07-29 22:48:46 +0200

    Extends rebuild and adds again a stateful widget including a listview which holds the single items

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 void main() {
-  runApp(const Backomat());
+  runApp(Backomat(worklistitems: [
+    WorkListItem(
+        step: 1,
+        name: 'Sauerteig ansetzen',
+        description: 'Sauerteig mit genügen Mehl ansetzen',
+        start: DateTime.now().add(const Duration(hours: 2)),
+        end: DateTime.now().add(const Duration(hours: 2))),
+    WorkListItem(
+        step: 2,
+        name: 'Sauerteig ansetzen',
+        description: 'Sauerteig mit genügen Mehl ansetzen',
+        start: DateTime.now().add(const Duration(hours: 4)),
+        end: DateTime.now().add(const Duration(hours: 4)))
+  ]));
 }
 
-class Backomat extends StatelessWidget {
-  const Backomat({super.key});
+class Backomat extends StatefulWidget {
+  const Backomat({required this.worklistitems, super.key});
+
+  final List<WorkListItem> worklistitems;
+
+  @override
+  State<Backomat> createState() => _WorkListState();
+}
+
+class _WorkListState extends State<Backomat> {
+  final _workList = <WorkListItem>{};
 
   void _addWorkListItem() {}
 
@@ -17,39 +40,45 @@ class Backomat extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Backomat'),
         ),
-        body: Center(
-          child: WorkListItem(
-              name: 'do something', start: DateTime.now(), end: DateTime.now()),
-        ),
+        body: ListView(children: widget.worklistitems.toList()),
         floatingActionButton: FloatingActionButton(
           onPressed: _addWorkListItem,
           tooltip: 'Increment',
           child: const Icon(Icons.add),
         ), // This trailing comma makes auto-formatting nicer for build methods.
+        ),
       ),
     );
   }
 }
 
 class WorkListItem extends StatelessWidget {
-  WorkListItem({
-    required this.name,
-    required this.start,
-    required this.end,
-  }) : super(key: ObjectKey(name));
+  WorkListItem(
+      {required this.step,
+      required this.name,
+      required this.description,
+      required this.start,
+      required this.end,
+      this.predecessor})
+      : super(key: ObjectKey(name));
 
+  final int step;
   final String name;
+  final String? description;
   final DateTime start;
   final DateTime end;
+  final WorkListItem? predecessor;
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      leading: const CircleAvatar(
+      leading: CircleAvatar(
         backgroundColor: Colors.red,
-        child: Text("1"),
+        child: Text(step.toString()),
       ),
-      title: Text(name + start.toIso8601String() + end.toIso8601String()),
+      title: Text("$name     ${DateFormat('E HH:mm').format(start)}"),
+      subtitle: Text(description!),
+      trailing: const Icon(Icons.more_vert),
     );
   }
 }
